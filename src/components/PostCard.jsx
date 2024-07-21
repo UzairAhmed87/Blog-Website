c93de86f53f8ede7e959ac660fdeddab2ea5731e
Uzair Ahmed <uzairahmed1574@gmail.com>
2024-07-22 02:26:17 +0500

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -32,7 +32,7 @@ export default function PostCard({ $id, title, featuredImage }) {
 
     return (
         <Link to={`/post/${$id}`}>
-            <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full bg-gray-400 rounded-xl p-4'>
                 <div className='w-full justify-center mb-4'>
                     {filePreview ? (
                         <img src={filePreview} alt="Featured" />
